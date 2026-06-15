915df542c5b1959f526cd60ba26037415fd58052
josebalilla1999-gif <josebalilla1999@gmail.com>
2026-06-15 14:30:01 +0200

    15-Jun v1.1.2 Routing

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { Login } from './login/login';
+import { Signin } from './signin/signin';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: App
+    },{
+        path: 'login',
+        component: Login
+    },{
+        path: 'signin',
+        component: Signin
+    }
+];
